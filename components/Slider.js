c6f4aea219d8762fb8a239753688b82cb2849bbb
JoeNguyen93 <joe.nguyen@softagram.com>
2017-12-05 19:27:27 +0200

    Improve Slider component

--- components/Slider.js
+++ components/Slider.js
@@ -3,26 +3,58 @@ import Carousel from 'react-native-snap-carousel';
 import PropTypes from 'prop-types';
 import { viewportWidth } from '../utils';
 
-const Slider = ({ items, renderItem }) => {
+const Slider = ({
+  items,
+  renderItem,
+  activeSlideAlignment,
+  sliderWidth,
+  itemWidth,
+  firstItem,
+  inactiveSlideScale,
+  inactiveSlideOpacity,
+  onSlideToItem,
+  onLoadCarousel
+}) => {
   return (
     <Carousel
       data={items}
+      ref={onLoadCarousel}
       renderItem={renderItem}
-      activeSlideAlignment="start"
-      sliderWidth={viewportWidth}
-      itemWidth={viewportWidth / 2.2}
-      firstItem={0}
-      inactiveSlideScale={1}
-      inactiveSlideOpacity={1}
+      activeSlideAlignment={activeSlideAlignment}
+      sliderWidth={sliderWidth}
+      itemWidth={itemWidth}
+      firstItem={firstItem}
+      inactiveSlideScale={inactiveSlideScale}
+      inactiveSlideOpacity={inactiveSlideOpacity}
       enableMomentum={true}
       decelerationRate={0.9}
+      onSnapToItem={onSlideToItem}
     />
-  )
+  );
 };
 
 Slider.propTypes = {
   items: PropTypes.array.isRequired,
-  renderItem: PropTypes.func.isRequired
+  renderItem: PropTypes.func.isRequired,
+  activeSlideAlignment: PropTypes.string,
+  sliderWidth: PropTypes.number,
+  itemWidth: PropTypes.number,
+  firstItem: PropTypes.number,
+  inactiveSlideScale: PropTypes.number,
+  inactiveSlideOpacity: PropTypes.number,
+  onSlideToItem: PropTypes.func,
+  onLoadCarousel: PropTypes.func
+};
+
+Slider.defaultProps = {
+  activeSlideAlignment: 'start',
+  sliderWidth: viewportWidth,
+  itemWidth: viewportWidth / 2.2,
+  firstItem: 0,
+  inactiveSlideScale: 1,
+  inactiveSlideOpacity: 1,
+  onSlideToItem: () => {},
+  onLoadCarousel: () => {}
 };
 
 export default Slider;
