6c68418c1bf3119c6a0403d2810fca1f26e5098d
JoeNguyen93 <joe.nguyen@softagram.com>
2017-12-05 19:26:19 +0200

    Improve Profile Screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,12 +1,109 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import { List, ListItem } from 'react-native-elements';
+import PropTypes from 'prop-types';
 
-const ProfileScreen = () => {
+const profileList = [
+  {
+    name: 'Daisy Duck',
+    avatar_url:
+      'https://vignette.wikia.nocookie.net/disney/images/f/f3/Daisy_Duck_375.jpg/revision/latest?cb=20131102202354',
+    subtitle: 'View my profile'
+  }
+];
+
+const settingList1 = [
+  {
+    title: 'Notifications',
+    icon: 'notifications'
+  },
+  {
+    title: 'Settings',
+    icon: 'settings'
+  },
+  {
+    title: 'Contact us',
+    icon: 'forum'
+  }
+];
+
+const settingList2 = [
+  {
+    title: 'Log out',
+    icon: 'power-settings-new'
+  }
+];
+
+const ProfileScreen = ({ onPressLogout }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile Screen</Text>
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <List containerStyle={{ marginTop: 50 }}>
+          {profileList.map((l, i) => (
+            <ListItem
+              roundAvatar
+              avatar={{ uri: l.avatar_url }}
+              key={i}
+              title={l.name}
+              subtitle={l.subtitle}
+              onPress={() => {}}
+            />
+          ))}
+        </List>
+        <List>
+          {settingList1.map((item, i) => (
+            <ListItem
+              key={i}
+              title={item.title}
+              leftIcon={{ name: item.icon }}
+              onPress={() => {}}
+            />
+          ))}
+        </List>
+        <List>
+          {settingList2.map((item, i) => (
+            <ListItem
+              key={i}
+              title={item.title}
+              titleStyle={{ color: '#cd3838', fontSize: 18 }}
+              leftIcon={{ name: item.icon }}
+              onPress={onPressLogout}
+            />
+          ))}
+        </List>
+      </ScrollView>
     </View>
-  )
+  );
 };
+
+ProfileScreen.propTypes = {
+  onPressLogout: PropTypes.func
+};
+
+ProfileScreen.defaultProps = {
+  onPressLogout: () => {}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#F1F2F2'
+  },
+
+  contentContainer: {
+    paddingVertical: 20
+  },
+
+  contents: {
+    marginTop: 50
+  }
+});
 
 export default ProfileScreen;
--- screens/ProfileScreenContainer.js
+++ screens/ProfileScreenContainer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ProfileScreen from './ProfileScreen';
 import { Icon } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { logout } from '../actions/auth';
+import { NavigationActions } from 'react-navigation';
 
 class ProfileScreenContainer extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -8,14 +11,25 @@ class ProfileScreenContainer extends Component {
     headerStyle: { backgroundColor: '#50C9BA' },
     headerTitleStyle: { color: 'white' },
     tabBarIcon: ({ tintColor }) => (
-      <Icon name='gear' type='font-awesome' color={tintColor}/>
+      <Icon name="gear" type="font-awesome" color={tintColor} />
     ),
     tabBarLabel: 'Profile'
   });
 
+  onPressLogout = async () => {
+    const { status } = await this.props.logout();
+    if (status === 'success') {
+      NavigationActions.reset({
+        index: 0,
+        key: null,
+        actions: [this.props.navigation.navigate('loginContainer')]
+      });
+    }
+  };
+
   render() {
-    return <ProfileScreen />;
+    return <ProfileScreen onPressLogout={this.onPressLogout} />;
   }
 }
 
-export default ProfileScreenContainer;
+export default connect(null, { logout })(ProfileScreenContainer);
