c3d7ef87635f299892df8acddf4759b0f86cc7a8
JoeNguyen93 <joe.nguyen@softagram.com>
2017-12-05 19:26:05 +0200

    Improve Map Screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,12 +1,116 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { MapView } from 'expo';
+import Slider from '../components/Slider';
+import PropTypes from 'prop-types';
+import CaseCard from '../components/CaseCard';
+import { Icon } from 'react-native-elements';
 
-const MapScreen = () => {
+const MapScreen = ({
+  onLoadMap,
+  onLoadCarousel,
+  onSlideToCard,
+  renderItem,
+  items,
+  initialRegion,
+  data,
+  onPressMarker
+}) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Map Screen</Text>
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={initialRegion}
+        ref={onLoadMap}
+      >
+        {data.markers.map((marker, index) => {
+          return (
+            <MapView.Marker key={index} coordinate={marker.coordinate}>
+              <TouchableOpacity style={styles.markerContainerStyle} onPress={() => onPressMarker(index) }>
+                <Icon
+                  name="user-secret"
+                  type="font-awesome"
+                  color="white"
+                  containerStyle={{ backgroundColor: '#4BA2AC' }}
+                />
+              </TouchableOpacity>
+            </MapView.Marker>
+          );
+        })}
+      </MapView>
+      <View style={styles.cardSliderContainer}>
+        <Slider
+          items={items}
+          onLoadCarousel={onLoadCarousel}
+          renderItem={renderItem}
+          inactiveSlideOpacity={0.7}
+          inactiveSlideScale={0.9}
+          activeSlideAlignment="center"
+          firstItem={1}
+          onSlideToItem={onSlideToCard}
+        />
+      </View>
     </View>
-  )
+  );
 };
+
+MapScreen.propTypes = {
+  onLoadCarousel: PropTypes.func,
+  onLoadMap: PropTypes.func,
+  onSlideToCard: PropTypes.func,
+  renderItem: PropTypes.func.isRequired,
+  items: PropTypes.array.isRequired,
+  data: PropTypes.object,
+  initialRegion: PropTypes.object,
+  onPressMarker: PropTypes.func
+};
+
+MapScreen.defaultProps = {
+  onLoadCarousel: () => {},
+  onLoadMap: () => {},
+  onSlideToCard: () => {},
+  onPressMarker: () => {}
+};
+
+const styles = StyleSheet.create({
+  cardSliderContainer: {
+    position: 'absolute',
+    bottom: 0
+  },
+  markerWrap: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  marker: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: 'rgba(130,4,150, 0.9)'
+  },
+  ring: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: 'rgba(130,4,150, 0.3)',
+    position: 'absolute',
+    borderWidth: 1,
+    borderColor: 'rgba(130,4,150, 0.5)'
+  },
+  markerContainerStyle: {
+    borderRadius: 50,
+    height: 36,
+    width: 36,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4BA2AC',
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 2,
+    shadowOpacity: 0.5
+  }
+});
 
 export default MapScreen;
--- screens/MapScreenContainer.js
+++ screens/MapScreenContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MapScreen from './MapScreen';
 import { Icon } from 'react-native-elements';
+import CaseCard from '../components/CaseCard';
 
 class MapScreenContainer extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -8,13 +9,81 @@ class MapScreenContainer extends Component {
     headerStyle: { backgroundColor: '#50C9BA' },
     headerTitleStyle: { color: 'white' },
     tabBarIcon: ({ tintColor }) => (
-      <Icon name='globe' type='font-awesome' color={tintColor} />
+      <Icon name="globe" type="font-awesome" color={tintColor} />
     ),
     tabBarLabel: 'Map'
   });
 
+  state = {
+    markers: [
+      {
+        coordinate: {
+          latitude: 45.524548,
+          longitude: -122.6749817
+        },
+        title: 'Best Place',
+        description: 'This is the best place in Portland'
+      },
+      {
+        coordinate: {
+          latitude: 45.524698,
+          longitude: -122.6655507
+        },
+        title: 'Second Best Place',
+        description: 'This is the second best place in Portland'
+      },
+      {
+        coordinate: {
+          latitude: 45.5230786,
+          longitude: -122.6701034
+        },
+        title: 'Third Best Place',
+        description: 'This is the third best place in Portland'
+      },
+      {
+        coordinate: {
+          latitude: 45.521016,
+          longitude: -122.6561917
+        },
+        title: 'Fourth Best Place',
+        description: 'This is the fourth best place in Portland'
+      }
+    ],
+    region: {
+      latitude: 45.52220671242907,
+      longitude: -122.6653281029795,
+      latitudeDelta: 0.04864195044303443,
+      longitudeDelta: 0.040142817690068
+    }
+  };
+
+  items = [0, 1, 2, 3];
+
+  onSlideToCard = (index) => {
+    this.map.animateToRegion({
+      ...this.state.markers[index].coordinate,
+      latitudeDelta: 0.02,
+      longitudeDelta: 0.02
+    }, 200);
+  };
+
+  onPressMarker = (index) => {
+    this.carousel.snapToItem(index);
+  };
+
   render() {
-    return <MapScreen/>;
+    return (
+      <MapScreen
+        items={this.items}
+        data={this.state}
+        initialRegion={this.state.region}
+        renderItem={({ item }) => <CaseCard caseInfo={{}} />}
+        onLoadCarousel={carousel => this.carousel = carousel }
+        onLoadMap={map => this.map = map }
+        onSlideToCard={this.onSlideToCard}
+        onPressMarker={this.onPressMarker}
+      />
+    );
   }
 }
 
