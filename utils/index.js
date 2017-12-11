ccca7cbf5aa3c35005a8495c2a54c6c9679f780f
JoeNg93 <ntuandung93@gmail.com>
2017-12-11 03:01:42 +0200

    Add util to change an object to an arr including its keys

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,5 @@
 import { Dimensions } from 'react-native';
+import _ from 'lodash';
 
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get(
   'window'
@@ -8,4 +9,7 @@ const wp = percentage => Math.round(percentage * viewportWidth / 100);
 
 const hp = percentage => Math.round(percentage * viewportHeight / 100);
 
-export { viewportWidth, viewportHeight, wp, hp };
+const objToArrIncludingKey = obj =>
+  _.map(obj, (val, key) => ({ id: key, ...val }));
+
+export { viewportWidth, viewportHeight, wp, hp, objToArrIncludingKey };
