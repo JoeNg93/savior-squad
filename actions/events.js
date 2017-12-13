7920f57a7f995d0f4198ab4748a138fb41101644
JoeNg93 <ntuandung93@gmail.com>
2017-12-14 01:49:47 +0200

    Add action creators for save/unsave events

--- actions/events.js
+++ actions/events.js
@@ -13,6 +13,15 @@ export const LEAVE_EVENT_SUCCESS = 'LEAVE_EVENT_SUCCESS';
 export const LEAVE_EVENT_FAIL = 'LEAVE_EVENT_FAIL';
 
 export const SET_SELECTED_EVENT_ID = 'SET_SELECTED_EVENT_ID';
+
+export const SAVE_EVENT_PENDING = 'SAVE_EVENT_PENDING';
+export const SAVE_EVENT_SUCCESS = 'SAVE_EVENT_SUCCESS';
+export const SAVE_EVENT_FAIL = 'SAVE_EVENT_FAIL';
+
+export const UNSAVE_EVENT_PENDING = 'UNSAVE_EVENT_PENDING';
+export const UNSAVE_EVENT_SUCCESS = 'UNSAVE_EVENT_SUCCESS';
+export const UNSAVE_EVENT_FAIL = 'UNSAVE_EVENT_FAIL';
+
 export const getAllEvents = () => async dispatch => {
   dispatch({ type: GET_EVENTS_PENDING });
   firebase
@@ -61,3 +70,35 @@ export const setSelectedEventId = eventId => ({
   type: SET_SELECTED_EVENT_ID,
   payload: eventId
 });
+
+export const saveEvent = eventId => async (dispatch, getState) => {
+  dispatch({ type: SAVE_EVENT_PENDING });
+  const state = getState();
+  const { allEvents } = state.events;
+  const { currentUser } = state.auth;
+  try {
+    await firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/events/${eventId}`)
+      .set(allEvents[eventId]);
+    dispatch({ type: SAVE_EVENT_SUCCESS });
+    return { status: 'success' };
+  } catch (err) {
+    dispatch({ type: SAVE_EVENT_FAIL });
+    return { status: 'fail' };
+  }
+};
+
+export const unsaveEvent = eventId => async (dispatch, getState) => {
+  dispatch({ type: UNSAVE_EVENT_PENDING });
+  const state = getState();
+  const { currentUser } = state.auth;
+  try {
+    await firebase.database().ref(`/users/${currentUser.uid}/events/${eventId}`).remove();
+    dispatch({ type: UNSAVE_EVENT_SUCCESS });
+    return { status: 'success' };
+  } catch (err) {
+    dispatch({ type: UNSAVE_EVENT_FAIL });
+    return { success: 'fail' };
+  }
+};
