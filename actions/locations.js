4bf8c38b023566a26565ac2b8d6a25c7b2b27ce3
JoeNg93 <ntuandung93@gmail.com>
2017-12-14 01:54:35 +0200

    Add action creators to start/stop listen to user's location

--- actions/locations.js
+++ actions/locations.js
@@ -12,6 +12,12 @@ export const GET_USER_LOCATION_FOR_TRACKING_SUCCESS =
 export const GET_USER_LOCATION_FOR_TRACKING_FAIL =
   'GET_USER_LOCATION_FOR_TRACKING_FAIL';
 
+export const STOP_LOCATION_TRACKING_PENDING = 'STOP_LOCATION_TRACKING_PENDING';
+export const STOP_LOCATION_TRACKING_SUCCESS = 'STOP_LOCATION_TRACKING_SUCCESS';
+export const STOP_LOCATION_TRACKING_FAIL = 'STOP_LOCATION_TRACKING_FAIL';
+
+export const UPDATE_CURRENT_LOCATION = 'UPDATE_CURRENT_LOCATION';
+
 export const getCurrentLocation = () => async dispatch => {
   dispatch({ type: GET_CURRENT_LOCATION_PENDING });
   try {
@@ -28,4 +34,63 @@ export const getCurrentLocation = () => async dispatch => {
   }
 };
 
-export const listenToLocationChange = () => async dispatch => {};
+export const listenToLocationChange = eventId => async (dispatch, getState) => {
+  dispatch({ type: GET_USER_LOCATION_FOR_TRACKING_PENDING });
+  const state = getState();
+  const { currentUser } = state.auth;
+  const userId = currentUser.uid;
+  const locationListenerRef = await Location.watchPositionAsync(
+    { enableHighAccuracy: true, distanceInterval: 2 },
+    async ({ coords }) => {
+      const { latitude, longitude } = coords;
+      dispatch({
+        type: UPDATE_CURRENT_LOCATION,
+        payload: { latitude, longitude }
+      });
+
+      await firebase
+        .database()
+        .ref(`/events/${eventId}/participants/${userId}/location`)
+        .set({ latitude, longitude });
+
+      // await firebase
+      //   .database()
+      //   .ref(`/events/${eventId}/participants/${userId}/trackingLocs`)
+      //   .push({ latitude, longitude });
+    }
+  );
+  dispatch({
+    type: GET_USER_LOCATION_FOR_TRACKING_SUCCESS,
+    payload: locationListenerRef
+  });
+};
+
+export const stopListeningToLocationChange = eventId => async (
+  dispatch,
+  getState
+) => {
+  dispatch({ type: STOP_LOCATION_TRACKING_PENDING });
+  const state = getState();
+  const { currentUser } = state.auth;
+  const userId = currentUser.uid;
+  const currentEvent = state.events.allEvents[eventId];
+  const { locationListenerRef } = state.locations;
+  if (locationListenerRef) {
+    locationListenerRef.remove();
+
+    await firebase
+      .database()
+      .ref(`/events/${eventId}/participants/${userId}/trackingLocs`)
+      .remove();
+
+    await firebase
+      .database()
+      .ref(`/events/${eventId}/participants/${userId}/location`)
+      .remove();
+    dispatch({ type: STOP_LOCATION_TRACKING_SUCCESS });
+    return { status: 'success' };
+  } else {
+    dispatch({ type: STOP_LOCATION_TRACKING_FAIL });
+    return { status: 'fail' };
+  }
+};
