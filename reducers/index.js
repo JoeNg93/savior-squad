57fdea1fb077ff632bf5c6bd7ce0cc4919a0da69
JoeNg93 <ntuandung93@gmail.com>
2017-12-14 01:59:37 +0200

    Add reducer for controlling Grid View to main reducer

--- reducers/index.js
+++ reducers/index.js
@@ -3,10 +3,12 @@ import authReducer from './auth';
 import casesReducer from './cases';
 import eventsReducer from './events';
 import locationsReducer from './locations';
+import systemReducer from './system';
 
 export default combineReducers({
   auth: authReducer,
   cases: casesReducer,
   events: eventsReducer,
   locations: locationsReducer,
+  system: systemReducer
 });
